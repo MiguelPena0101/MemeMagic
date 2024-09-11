fac0968e7f29da2e56e4b742d9be349c5e07c23e
Miguel Pena <Miguel.Pena1709@gmail.com>
2024-09-10 20:14:47 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -6,4 +6,14 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: 'postgres',
   },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 };
